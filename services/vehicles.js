ef909617a3140e130697a4e2c501b9834d2569c0
Stéphane Mombuleau <mombul@pm.me>
2021-09-06 22:27:37 +0200

    Changed sql query

--- services/vehicles.js
+++ services/vehicles.js
@@ -3,7 +3,7 @@ const helper = require('../helper');
 
 async function getMultiple(){
   const rows = await db.query(
-    `SELECT currentCoordsX, currentCoordsY, plate FROM owned_vehicles`, 
+    `SELECT currentCoordsX, currentCoordsY, plate FROM owned_vehicles WHERE currentCoordsX IS NOT NULL`, 
     []
   );
   const data = helper.emptyOrRows(rows);
